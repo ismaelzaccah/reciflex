a9a2f3221421915304c50b8f68ac3faa7f902fdf
Ismael Zaccah <ismael.zaccah@gmail.com>
2021-09-30 18:03:45 -0300

    Reciflex 1.0

---
+++ app.js
@@ -0,0 +1,87 @@
+//+ Carlos R. L. Rodrigues
+//@ http://jsfromhell.com/string/extenso [rev. #3]
+
+String.prototype.extenso = function(c){
+	var ex = [
+		["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"],
+		["dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"],
+		["cem", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"],
+		["mil", "milhão", "bilhão", "trilhão", "quadrilhão", "quintilhão", "sextilhão", "setilhão", "octilhão", "nonilhão", "decilhão", "undecilhão", "dodecilhão", "tredecilhão", "quatrodecilhão", "quindecilhão", "sedecilhão", "septendecilhão", "octencilhão", "nonencilhão"]
+	];
+	var a, n, v, i, n = this.replace(c ? /[^,\d]/g : /\D/g, "").split(","), e = " e ", $ = "real", d = "centavo", sl;
+	for(var f = n.length - 1, l, j = -1, r = [], s = [], t = ""; ++j <= f; s = []){
+		j && (n[j] = (("." + n[j]) * 1).toFixed(2).slice(2));
+		if(!(a = (v = n[j]).slice((l = v.length) % 3).match(/\d{3}/g), v = l % 3 ? [v.slice(0, l % 3)] : [], v = a ? v.concat(a) : v).length) continue;
+		for(a = -1, l = v.length; ++a < l; t = ""){
+			if(!(i = v[a] * 1)) continue;
+			i % 100 < 20 && (t += ex[0][i % 100]) ||
+			i % 100 + 1 && (t += ex[1][(i % 100 / 10 >> 0) - 1] + (i % 10 ? e + ex[0][i % 10] : ""));
+			s.push((i < 100 ? t : !(i % 100) ? ex[2][i == 100 ? 0 : i / 100 >> 0] : (ex[2][i / 100 >> 0] + e + t)) +
+			((t = l - a - 2) > -1 ? " " + (i > 1 && t > 0 ? ex[3][t].replace("ão", "ães") : ex[3][t]) : ""));
+		}
+		a = ((sl = s.length) > 1 ? (a = s.pop(), s.join(" ") + e + a) : s.join("") || ((!j && (n[j + 1] * 1 > 0) || r.length) ? "" : ex[0][0]));
+		a && r.push(a + (c ? (" " + (v.join("") * 1 > 1 ? j ? d + "s" : (/0{6,}$/.test(n[0]) ? "de " : "") + $.replace("l", "is") : j ? d : $)) : ""));
+	}
+	return r.join(e);
+}
+
+//Fim do codigo de Terceiros--
+
+
+
+//Tela de Campos, Tela de impressão
+var screen          = document.querySelector("form")
+var printer         = document.querySelector("main")
+//Campos Digitaveis  
+var clientField     = document.querySelector("#clientField")
+var numField        = document.querySelector("#numField")
+var refTextField    = document.querySelector("#refTextField")
+//Botões
+var buttonRender    = document.querySelector("#render")
+var buttonCreateNew = document.querySelector("#createNew")
+var buttonPrint     = document.querySelector("#print")
+//Tags que receberão os dados ao renderizar
+var dateWrite       = document.querySelector("#today")
+var clientWrite     = document.querySelector("#clientWrite")
+var refTextWrite    = document.querySelector("#refTextWrite")
+var numExtWrite     = document.querySelector("#numExtWrite")
+var numWrite        = document.querySelector("#numWrite")
+//Data de hoje Automática 
+var today           = new Date()
+var monthExt        = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+var date            = `Fortaleza, ${today.getDate()} de ${monthExt[today.getMonth()]} de ${today.getFullYear()}`
+
+//Função para escrever os dados tratados nas tags
+function writeInTag(tagLoad,tagTarget) {
+	let text = document.createTextNode(tagLoad)
+	tagTarget.appendChild(text)
+}
+
+buttonCreateNew.addEventListener("click", ()=>{
+	window.location.reload()})
+	buttonPrint.addEventListener("click", ()=>{
+		window.print()})
+		
+//Ação do botão --Gerar Recibo-- da tela de Campos
+buttonRender.addEventListener("click", (event)=>{
+	event.preventDefault()
+	
+	//Remove epaços do do inicio/fim da string
+	var client = clientField.value.trim()
+	var refText = refTextField.value.trim()
+	var num = numField.value.trim()
+	
+	//trata e converte o valor para extenso
+	num = num.replace(".", ",")
+	var numExt = num.extenso(currency = true)
+	num = "R$ " + num
+
+	writeInTag(client, clientWrite)	
+	writeInTag(refText, refTextWrite)
+	writeInTag(num, numWrite)
+	writeInTag(numExt, numExtWrite)
+	writeInTag(date, dateWrite)
+	
+	screen.classList.add("off")
+	printer.classList.remove("off")
+})
